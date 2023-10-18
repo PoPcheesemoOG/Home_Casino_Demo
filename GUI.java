725a71b75e1b5714b5217c00c9ade506de1b2dd0
Paul Helske <116106386+PoPcheesemoOG@users.noreply.github.com>
2023-10-17 18:10:19 -0600

    Add files via upload

--- GUI.java
+++ GUI.java
@@ -1,6 +1,8 @@
 /* Name: Paul Helske
  * Date: 10/17/2023
  */
+
+import java.util.*;
 import javafx.scene.layout.*;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
@@ -11,11 +13,12 @@ import javafx.scene.*;
 
 public class GUI extends Application {
 
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		VBox vBox = new VBox(10);
 		Scene menuScene = new Scene(vBox, 700, 600);
-	
+
 		MenuBar menuBar = new MenuBar();		
 		Menu menuGames = new Menu("Games");		
 		menuBar.getMenus().add(menuGames);
@@ -23,13 +26,43 @@ public class GUI extends Application {
 		MenuItem menuItemPlayPoker = new MenuItem("Play Poker");
 		MenuItem menuItemPlayKeno = new MenuItem("Play Keno");
 		MenuItem menuItemExit = new MenuItem("Exit");
-	
+
 		menuGames.getItems().addAll(menuItemPlayPoker, menuItemPlayKeno, menuItemExit);
-		
+
 		menuItemPlayPoker.setOnAction(e -> newPoker());
 		menuItemPlayKeno.setOnAction(e -> newKeno());
 		menuItemExit.setOnAction(e -> System.exit(0));
 
+		List<GameCard> deck = new ArrayList<GameCard>();
+		
+		for (int i = 0; i < 52; i++) {
+			deck.add(new GameCard());
+		}
+		for (int i = 0; i < 4; i++) {
+			for (int j = 1; j < 14; j++) {
+				switch (i) {
+				case 0: deck.get(j - 1).setName("Spade" + j);
+						deck.get(j - 1).setNumber(j);
+						deck.get(j - 1).setImage("Users/student/eclipse-workspace/VideoPoker/spade" + j + ".png");
+						break;
+				case 1: deck.get(j + 12).setName("Club" + j);
+						deck.get(j + 12).setNumber(j);
+						deck.get(j + 12).setImage("/Cards/club" + j + ".png");
+						break;
+				case 2: deck.get(j + 25).setName("Heart" + j);
+						deck.get(j + 25).setNumber(j);
+						deck.get(j + 25).setImage("/Cards/heart" + j + ".png");
+						break;
+				case 3: deck.get(j + 38).setName("Diamond" + j);
+						deck.get(j + 38).setNumber(j);
+						deck.get(j + 38).setImage("/Cards/diamond" + j + ".png");
+						break;
+				}
+			}
+		}
+		for (int i = 0; i < 52; i++) {
+			System.out.println(deck.get(i).getName());
+		}
 		vBox.getChildren().addAll(menuBar);
 		primaryStage.setTitle("Home Casino Main Menu");
 		primaryStage.setScene(menuScene);
@@ -44,16 +77,16 @@ public class GUI extends Application {
 		MenuItem menuItemExit1 = new MenuItem("Exit");
 		menuBarPoker.getMenus().add(menuExit1);
 		menuExit1.getItems().add(menuItemExit1);
-					
+
 		VBox vBox1 = new VBox(10);
 		vBox1.getChildren().addAll(menuBarPoker);
-		
+
 		Scene pokerScene = new Scene(vBox1, 600, 400);
 		Stage pokerStage = new Stage();
 		pokerStage.setTitle("Video Poker");
 		pokerStage.setScene(pokerScene);
 		pokerStage.show();
-		
+
 		menuItemExit1.setOnAction(e1 -> pokerStage.close());
 	}
 	private void newKeno() {
@@ -62,16 +95,16 @@ public class GUI extends Application {
 		MenuItem menuItemExit1 = new MenuItem("Exit");
 		menuBarKeno.getMenus().add(menuExit1);
 		menuExit1.getItems().add(menuItemExit1);
-					
+
 		VBox vBox1 = new VBox(10);
 		vBox1.getChildren().addAll(menuBarKeno);
-		
+
 		Scene kenoScene = new Scene(vBox1, 600, 400);
 		Stage kenoStage = new Stage();
 		kenoStage.setTitle("Card Keno");
 		kenoStage.setScene(kenoScene);
 		kenoStage.show();
-		
+
 		menuItemExit1.setOnAction(e1 -> kenoStage.close());
 	}
 }
--- GameCard.java
+++ GameCard.java
@@ -6,8 +6,34 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class GameCard {
-	public Image img;
-	public ImageView view = new ImageView(img);
+//	public Image img;
+	public String img;
+//	public ImageView view = new ImageView(img);
+	public String view = new String();
 	public enum Suit {Spade, Clubs, Heart, Diamond};
 	public int Number;
+	public String name;
+
+	public GameCard() {
+		
+	}
+	public GameCard(String name1) {
+		name = name1;
+	}
+	
+	public void setName(String name1) {
+		name = name1;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setNumber(int Number1) {
+		Number = Number1;
+	}
+	public int getNumber() {
+		return Number;
+	}
+	public void setImage(String location) {
+		img = location;
+	}
 }
